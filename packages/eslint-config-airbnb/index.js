bb05b3290ed10b109016021828940df5676c8f4d
Елена Пушкина <l-e-n-o-k@mail.ru>
2022-02-25 12:55:10 +0300

    branch1 in other 3

--- packages/eslint-config-airbnb/index.js
+++ packages/eslint-config-airbnb/index.js
@@ -2,7 +2,7 @@ module.exports = {
   extends: [
     'eslint-config-airbnb-based',
     './canon/node',
-    './canon/node-a11y',
+    './canon/node--a11y',
   ].map(require.resolve),
   rules: {}
 };
